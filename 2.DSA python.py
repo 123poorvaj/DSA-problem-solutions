8ffaad4636e0b5e4c130b9ca3f70cb962be35e39
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-06-26 18:52:18 +0530

    Add files via upload

---
+++ 2.DSA python.py
@@ -0,0 +1,10 @@
+#find sum of all the elements in a list
+#A is a list it stores the some numbers
+#for loop is use to itarate the sum  variabel  and it helps essey way to sum the numbers
+
+A=[10,20,30,40,50,70]
+sum=0
+for i in range (0,len(A)):
+    sum=sum+A[i]
+print(sum)
+
