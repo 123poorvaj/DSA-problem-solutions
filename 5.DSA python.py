84c2b407473d11d14285eddd9c1e87081ae8123b
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-07-01 20:00:16 +0530

    Add files via upload

---
+++ 5.DSA python.py
@@ -0,0 +1,8 @@
+#tupels
+#tuples are almost like a list
+# but tupels is change the value ,
+# in list you can change
+# tuple created using open bracket#
+a=(1,3,4,5,2,3,"hdhgf")
+#a[3]=4 this line show the error becouse tuples not change the value
+print(a)
