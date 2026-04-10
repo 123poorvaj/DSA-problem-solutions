e1f88b324ad4abe4a0e967899f30108797d27193
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-04-11 00:35:51 +0530

    Implement user login functionality with validation

--- LOGIN PROGECT/LOGIN.py
+++ LOGIN PROGECT/LOGIN.py
@@ -1,1 +1,27 @@
+a=input("Enter your name >> ")
+b=input("Enter the password >> ")
+userDetails={"kishore":"ki@123","poorvaj":"poorvaj@134","Adi":"Adi@321"}
 
+LOGIN=False
+userName=False
+password=False
+
+
+
+if (a,b) in userDetails.items():
+    LOGIN=True
+else :
+    if a not in userDetails.keys():
+        userName=True
+    elif userDetails[a]!=b: 
+        password=True
+        
+            
+
+if LOGIN:
+    print("******Login Successfull****")
+
+if userName:
+    print(" please check  you user name is Incorrect")
+if password:
+    print("please  check  your password is Incorrect")
